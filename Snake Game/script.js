3df9066bbffca4192de116fc58b2e3ac8cba71c2
David Pinner <confused212@Davids-MacBook-Pro.local>
2018-12-04 15:06:12 +0300

    updated snake functionality

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -1,21 +1,45 @@
 var snakeCanvas = document.querySelector('#snake-canvas').getContext('2d');
 
 var snakeList, foodList;
-var width = 500;
-var height = 500;
-snakeCanvas.font = '20px Calibri';
+var WIDTH = 500;
+var HEIGHT = 500;
+snakeCanvas.font = '20px Ariel';
 
 var snakeBody = {
     width: 20,
-    height: 20
+    height: 20,
+    color: 'green'
 };
 
 var snakeFood = {
     width: 20,
-    height: 20
+    height: 20,
+    color: 'blue'
 };
 
-var startGame = function() {
+function snake(sb, i) {
+    snakeCanvas.save();
+    if (i == 0) {
+        snakeCanvas.fillStyle = 'black';
+    } else {
+        snakeCanvas.fillStyle = snakeBody.color;
+        snakeCanvas.fillRect(sb.x, sb.y, snakeBody.width, snakeBody.height);
+
+        snakeCanvas.restore();
+
+    }
+};
+
+function food(f, i) {
+    snakeCanvas.save();
+
+    snakeCanvas.fillStyle = snakeFood.color;
+    snakeCanvas.fillRect(f.x, f.y, snakeFood.width, snakeFood.height);
+
+    snakeCanvas.restore();
+};
+
+function startGame() {
     snakeList = [
         { x: 220, y: 200 },
         { x: 210, y: 200 },
@@ -23,4 +47,8 @@ var startGame = function() {
     ];
 
     foodList = [];
+
+    snakeList.forEach(snake);
 };
+
+startGame();
