7a72016ab06cac7249c06527ee155e8db5789785
David Pinner <confused212@Davids-MacBook-Pro.local>
2018-12-07 15:36:46 +0300

    renamed pictures to make them easier to work with

---
+++ Javascript-practice/js/script.js
@@ -0,0 +1,10 @@
+var santaLogo = document.querySelector('.santa');
+
+santaLogo.addEventListener('mousedown', function() {
+    var randomPic = Math.floor(math.random() * 4);
+});
+
+
+// get santa to go to random position upon mouse down
+
+// santa to back to normal position on mouse up
