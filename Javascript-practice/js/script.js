73fff970558909522849baf5007cb671d704aabe
David Pinner <confused212@Davids-MacBook-Pro.local>
2018-12-07 18:52:35 +0300

    updated javascript to change P elements to diffrent fonts

--- Javascript-practice/js/script.js
+++ Javascript-practice/js/script.js
@@ -1,7 +1,8 @@
 var santaLogo = document.querySelector('.santa');
+var checked = document.querySelector('#dropdown');
 
+// santa logo to spin when clicked
 santaLogo.addEventListener('mousedown', function() {
-
     // creat random number to cycle through pictures
     var randomPic = Math.floor(Math.random() * 4);
 
@@ -11,8 +12,20 @@ santaLogo.addEventListener('mousedown', function() {
 });
 
 santaLogo.addEventListener('mouseup', function() {
-
     // santa to back to normal position on mouse up
     santaLogo.src = "imgs/santa-0.png";
 
 });
+
+// select check box menu and change font as per select item
+
+document.getElementById('font').addEventListener('change', function() {
+    // select the all P elements
+    var elems = document.querySelectorAll('p');
+
+    // loop through all P elements to update font
+    for (var i = 0; i < elems.length; i++) {
+        // update font
+        elems[i].style.fontFamily = this.value;
+    }
+}, false);
